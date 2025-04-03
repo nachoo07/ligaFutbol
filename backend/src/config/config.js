3ba0f3216e7ca7e0ae5c937785568cbc5ca3e926
Ignacio Skibski <nanoskibski@gmail.com>
2025-04-03 13:25:37 -0300

    Actualizar el puerto por defecto en la configuración del backend

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -2,5 +2,5 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const PORT = process.env.PORT || 4001;
+export const PORT = process.env.PORT || 4002;
 export const CONNECTION_STRING = process.env.CONNECTION_STRING
